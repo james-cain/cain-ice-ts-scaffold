213f55e689695ee3ba6507a5e007b2a75e527589
james-cain <m15960192140@163.com>
2018-11-05 18:08:31 +0800

    feat: init repo

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
+  configureWebpack: {
+    devtool: 'source-map'
+  },
+  devServer: {
+    port: 1575,
+    proxy: {
+    },
+  }
+}
